d25ea1234cab09d8a59f5db34ee822b8cc953b98
laurentiusn <launchrist@gmail.com>
2025-09-20 09:55:31 +0700

    feat(database): create and configure booking service table based on ERD

---
+++ database/migrations/2025_09_19_105901_create_booking_service_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_service', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('id_service')->constrained('service')->cascadeOnDelete();
+            $table->foreignUuid('id_booking')->constrained('booking')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_service');
+    }
+};
